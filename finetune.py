3086b467e33e02dcf237763b53a7c7fb19d44ca5
Daniel-Domițian Iuonac <dd.iuonac@gmail.com>
2022-04-25 15:36:27 +0300

    fix disp_true reference before assigment

--- finetune.py
+++ finetune.py
@@ -90,6 +90,8 @@ def train(imgL, imgR, disp_L):
 
     if args.cuda:
         imgL, imgR, disp_true = imgL.cuda(), imgR.cuda(), disp_L.cuda()
+    else:
+        disp_true = disp_L
 
     # ---------
     mask = (disp_true > 0)
